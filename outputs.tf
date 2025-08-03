c84114068916c2eebb99b80d321e68c3bb700c6b
Grant Moore <gmoore@Grants-MacBook-Pro.local>
2025-08-02 20:39:24 -0400

    remove outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,1 @@
 # modules/dynamodb_lambda/outputs.tf
-
-output "dynamodb_table_arn" {
-  description = "The ARN of the created DynamoDB table."
-  value       = aws_dynamodb_table.lambda_table.arn
-}
-
-output "lambda_function_arn" {
-  description = "The ARN of the created Lambda function."
-  value       = aws_lambda_function.my_lambda.arn
-}
